73cffa94878731dbde1e90e80f20015f6a11de92
Zachary Lentz <zlentz@slac.stanford.edu>
2025-07-15 02:00:02 -0700

    Automatic backup @ Tue Jul 15 02:00:02 PDT 2025

--- xcs/camviewer.sh
+++ xcs/camviewer.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 HUTCH=xcs
-/reg/g/pcds/pyps/config/${HUTCH}/camviewer/run_viewer.csh --instrument ${HUTCH} --pvlist /reg/g/pcds/pyps/config/${HUTCH}/camviewer.cfg --rate 5
+/reg/g/pcds/pyps/config/${HUTCH}/camviewer/run_viewer.csh --instrument ${HUTCH}  --pvlist /reg/g/pcds/pyps/config/${HUTCH}/camviewer.cfg --rate 5
